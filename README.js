c511c131a01a05412fbd8d6b9cca7f410118db75
Quan Le <quanle.ddk@gmail.com>
2019-03-29 16:06:31 +0700

    29/03.1

--- README.js
+++ README.js
@@ -1,2 +1,11 @@
 Quan Le Hello W0rld !!!
 https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/skeleton_website
+
+const MongoClient = require(‘mongodb’).MongoClient;
+const uri = "mongodb+srv://admin:<password>@mongodb-w27ma.gcp.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
